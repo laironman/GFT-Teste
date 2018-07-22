1fba2ffb910943e91298e08d76bdf6fcada521cd
Luciano Azevedo <llopesa@gmail.com>
2018-07-22 03:07:35 +0000

    Estrutura Inicial
    Committer:  Luciano Azevedo <llopesa@gmail.com>

---
+++ Gft.UnitTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Gft.UnitTest
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Gft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
